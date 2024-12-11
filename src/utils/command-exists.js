45f2b33e2c10a0cb458c2e01837a7a06d6a80d53
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 06:25:31 -0800

    Update command-exists.js

--- src/utils/command-exists.js
+++ src/utils/command-exists.js
@@ -1,17 +1,14 @@
 const checkForCommand = require("command-exists");
 
-/**
- * Returns whether the provided shell command is available
- * @param {string} command - Shell command to check for
- * @returns {Promise<boolean>} - Whether the command is available
- */
-async function commandExists(command) {
-	// The `command-exists` library throws an error if the command is not available. This function
-	// catches these errors and returns a boolean value instead
-	try {
+async function commandExists(command) 
+{
+	try 
+	{
 		await checkForCommand(command);
 		return true;
-	} catch (error) {
+	} 
+	catch (error) 
+	{
 		return false;
 	}
 }
