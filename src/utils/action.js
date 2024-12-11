e60c4222db277617aa7f1890fa8365d7e63ba9c0
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 06:23:40 -0800

    Update action.js

--- src/utils/action.js
+++ src/utils/action.js
@@ -1,22 +1,13 @@
 const { execSync } = require("child_process");
-
 const core = require("@actions/core");
-
 const RUN_OPTIONS_DEFAULTS = { dir: null, ignoreErrors: false, prefix: "" };
 
-/**
- * Returns the value for an environment variable. If the variable is required but doesn't have a
- * value, an error is thrown
- * @param {string} name - Name of the environment variable
- * @param {boolean} required - Whether an error should be thrown if the variable doesn't have a
- * value
- * @returns {string | null} - Value of the environment variable
- */
-function getEnv(name, required = false) {
+function getEnv(name, required = false) 
+{
 	const nameUppercase = name.toUpperCase();
 	const value = process.env[nameUppercase];
-	if (value == null) {
-		// Value is either not set (`undefined`) or set to `null`
+	if (value == null) 
+	{
 		if (required) {
 			throw new Error(`Environment variable "${nameUppercase}" is not defined`);
 		}
@@ -25,20 +16,14 @@ function getEnv(name, required = false) {
 	return value;
 }
 
-/**
- * Executes the provided shell command
- * @param {string} cmd - Shell command to execute
- * @param {{dir: string, ignoreErrors: boolean}} [options] - {@see RUN_OPTIONS_DEFAULTS}
- * @returns {{status: number, stdout: string, stderr: string}} - Output of the shell command
- */
-function run(cmd, options) {
+function run(cmd, options) 
+{
 	const optionsWithDefaults = {
 		...RUN_OPTIONS_DEFAULTS,
 		...options,
 	};
 
 	core.debug(cmd);
-
 	try {
 		const stdout = execSync(cmd, {
 			encoding: "utf8",
@@ -54,8 +39,11 @@ function run(cmd, options) {
 		core.debug(`Stdout: ${output.stdout}`);
 
 		return output;
-	} catch (err) {
-		if (optionsWithDefaults.ignoreErrors) {
+	} 
+	catch (err) 
+	{
+		if (optionsWithDefaults.ignoreErrors) 
+		{
 			const output = {
 				status: err.status,
 				stdout: err.stdout.trim(),
